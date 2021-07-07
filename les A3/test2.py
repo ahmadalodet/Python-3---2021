f1a60dd524de76a2f67a4448f717b7822eaf7ff1
ahmadalodet <ahmadalodet@gmail.com>
2021-07-07 10:52:48 +0200

    Create test2.py

---
+++ les A3/test2.py
@@ -0,0 +1,23 @@
+name = "ahmad";
+
+skill = 20;
+
+skill + 10;
+
+years = [2009, 1990, 2010];
+
+years2 = {2008, 1928, 2019};
+
+print (years);
+print (years2 );
+
+print (years[0])
+
+#for item in years :
+    #print(item);
+
+years [0] = 0;
+
+print (years);
+    
+    
