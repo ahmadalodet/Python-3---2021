30155165262ab9ba34fdc965a03617532a06293a
ahmadalodet <ahmadalodet@gmail.com>
2021-07-14 19:25:41 +0200

    Create traning1.py

---
+++ traning1.py
@@ -0,0 +1,32 @@
+#nums = [1,2,3,4,5,6]
+#my_list = []
+
+
+#for item in nums :
+#    my_list.append(item);
+    
+#print("my_list: ", my_list)
+
+#num[0] = 99
+
+#print("num: ", nums)
+
+
+#def copy_only_un_even(mylist):
+    #answer = [];
+   # for item in my_list:
+    #    if item %2 !=0:
+           # answer.append(item);
+ #   return answer_list;
+
+#numbers = [1, 44, 52, 56, 53435, 434, 14, 305]
+#un_even_number = copy_only_un_even(numbers);
+#print("un_even_numbers: ",un_even_numbers);
+#
+
+numbers = [1, 2, 3, 33,44,55,66,77]
+#new_numbers = [];
+
+number_3 = [item+10 for item in numbers]
+print("number_3: ", number_3)
+
