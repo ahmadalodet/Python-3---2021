0d709a784d9d6d60cac3276709208c66d2186a4d
ahmadalodet <ahmadalodet@gmail.com>
2021-07-27 14:15:12 +0200

    Create grafix_interface.py

---
+++ grafix_interface.py
@@ -0,0 +1,32 @@
+
+class Dog :
+    def __init__(self):
+        self.name = "dogeee"
+    def bark(self):       
+        print("hau")
+            
+    def barkEnglish(self):
+        print("haoooo")
+
+
+
+
+dog = Dog()
+dog2 = Dog()
+
+
+dog.bark()
+dog.barkEnglish()
+
+print (dog.name)
+
+
+
+
+
+
+
+
+
+
+
