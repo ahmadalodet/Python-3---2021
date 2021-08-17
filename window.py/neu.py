6bc41bc59cda5d6a0140e0f35a0344c5484b6021
ahmadalodet <ahmadalodet@gmail.com>
2021-08-17 14:04:54 +0200

    Update neu.py

--- window.py/neu.py
+++ window.py/neu.py
@@ -3,7 +3,11 @@ from tkinter import *
 root = Tk()
 root.title("ahmad")
 root.geometry('500x800+200+200')
-button = Button(text='youtube').pack()
+button = Button(text="""+.+.+.+.+.
+                        +.+.+.+.
+                        +.+.+
+                        +.+.
+                        +.""").pack()
 
 
 
